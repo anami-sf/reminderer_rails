1a2f5e51f07e0f46f1f047135f04482f1ec19700
anami823 <anami823@gmail.com>
2019-12-29 20:04:54 -0800

    Add json response to dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
 
   def index
+    @user = User.find_by(id: session[:current_user_id]) 
+    render json: @user
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.json { render json: @user }
   end
 end
