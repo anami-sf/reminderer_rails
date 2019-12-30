6ee96b8acc2845cf5f2278b6c97d2fb8113df5f2
anami823 <anami823@gmail.com>
2019-12-29 21:54:33 -0800

    Make dashboard route public

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,11 @@
 class DashboardController < ApplicationController
 
+  skip_before_action :authorized, only: [:index]
+
   def index
     @user = User.find_by(id: session[:current_user_id]) 
+    foo = @user.to_json
+    puts foo
     render json: @user
   end
 end
