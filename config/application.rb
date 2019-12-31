ef39354485e18fff02d18868242d49cab1580428
anami823 <anami823@gmail.com>
2019-12-30 18:50:26 -0800

    Replace wild-card character with origin address

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Remindererapi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'https://reminderer-app.herokuapp.com/'
         resource '*', 
           headers: :any, 
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
